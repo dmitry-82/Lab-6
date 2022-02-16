491ce445d2eb8c32d45ebf2e7bfcd5d506dd3ce8
dmitry-82 <dima-tis19@rambler.ru>
2022-02-16 21:27:30 +0300

    Добавьте файлы проекта.

---
+++ Lab 6.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Тарту дорог как город утрат";
+            string s = string.Empty;
+            string y = string.Empty;
+
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                if (a[i].ToString() != " ")
+                {
+                    s += a[i].ToString().ToLower();
+                }
+            }
+            for (int j = 0; j < a.Length; j++)
+            {
+                if (a[j].ToString() != " ")
+                {
+                    y += a[j].ToString().ToLower();
+                }
+            }
+            if (s == y)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }                 
+                Console.ReadKey();
+        }
+    }
+}   
+
---
+++ Lab 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string input = Console.ReadLine();
+            string[] str = input.Split(new Char[] {' ',});
+            int max = 0, index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length > max)
+                {
+                    max = str[i].Length;
+                    index = i;
+                }
+            }
+            Console.Write("Самое длинное слово: {0}", str[index]);
+            Console.ReadKey();
+        }
+    }
+}
